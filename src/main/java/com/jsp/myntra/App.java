d8e170d13f9f0e0b14e6f33f1d2f41294c53bdd4
Abhra <abhrabose000@gmail.com>
2023-12-13 13:40:03 +0530

    Project initiated

---
+++ src/main/java/com/jsp/myntra/App.java
@@ -0,0 +1,13 @@
+package com.jsp.myntra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
